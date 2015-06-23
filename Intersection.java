cf0edd5f02c427cb7732ae9e51cc153aacf08924
Aggelos Biboudis <biboudis@gmail.com>
2015-06-23 16:42:08 +0300

    Add subtyping and the dual static-for in Intersection.

--- Intersection.java
+++ Intersection.java
@@ -1,7 +1,10 @@
 
 // Question: Is this equivalent with a A&B type?
-abstract class Intersection <interface A, interface B> {
+abstract class Intersection <interface A, interface B> implements A, B {
     
     <R extends Object, A*>[m] for(R m (A): A.methods ; some R m (A): B.methods )
+    abstract R m (A a);
+
+    <R extends Object, A*>[m] for(R m (A): B.methods ; some R m (A): A.methods )
     abstract R m (A a);
 }
