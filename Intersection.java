ff7bb36714fdb9a203fae37de692c6306f259254
Aggelos Biboudis <biboudis@gmail.com>
2015-06-23 16:52:12 +0300

    Remove morphing.

--- Intersection.java
+++ Intersection.java
@@ -1,10 +1,5 @@
 
 // Question: Is this equivalent with a A&B type?
-abstract class Intersection <interface A, interface B> implements A, B {
+interface Intersection <interface A, interface B> extends A, B {
     
-    <R extends Object, A*>[m] for(R m (A): A.methods ; some R m (A): B.methods )
-    abstract R m (A a);
-
-    <R extends Object, A*>[m] for(R m (A): B.methods ; some R m (A): A.methods )
-    abstract R m (A a);
 }
